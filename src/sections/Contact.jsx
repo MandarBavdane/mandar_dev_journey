6553388cb6b80007930a45e9e15b30de77f51078
Mandar Bavdane <mandarbavdane@gmail.com>
2025-04-02 20:26:31 +0530

    first commit

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,124 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  // service_so8d0ou
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_so8d0ou",
+        "template_vatq1we",
+        {
+          from_name: form.name,
+          to_name: "Mandar Bavdane",
+          from_email: form.email,
+          to_email: "mandarbavdane@gmail.com",
+          message: form.message,
+        },
+        "KeecYtv-P25qOAGQR"
+      );
+      setLoading(false);
+
+      alert("Your Message has been sent successfully!");
+
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setLoading(false);
+
+      console.log(error);
+    }
+  };
+
+  return (
+    <section className="c-space my-20" id="contact">
+      <div className="relative min-h-screen flex items-center justify-center flex-col">
+        <img
+          src="/assets/terminal.png"
+          alt="terminal background"
+          className="absolute inset-0 min-h-screen"
+        />
+        <div className="contact-container pt-10">
+          <h3 className="head-text">Let&apos;s Connect</h3>
+          <p className="text-lg text-white-600 mt-3">
+            Whether you&apos;re looking to build a new website, improve your
+            existing platform or hiring a unique project to life, I&apos;m here
+            to help.
+          </p>
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col space-y-7"
+          >
+            <label className="space-y-3">
+              <span className="field-label">Full Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="Mandar Bavdane"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                required
+                className="field-input"
+                placeholder="mandarbavdane@gmail.com"
+              />
+            </label>
+            <label className="space-y-3">
+              <span className="field-label">Your message</span>
+              <textarea
+                type="text"
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                required
+                rows={5}
+                className="field-input"
+                placeholder="Hello, I am interested in working with you..."
+              />
+            </label>
+            <button className="field-btn" type="submit" disabled={loading}>
+              {loading ? "Sending..." : "Send Message"}
+              <img
+                src="/assets/arrow-up.png"
+                alt="arrow-up"
+                className="field-btn_arrow"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <section className="c-space pt-7 pb-5 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      <div className="text-white-500 flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+
+      <div className="flex gap-3">
+        <a className="social-icon" href="https://github.com/MandarBavdane">
+          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+        </a>
+        <a className="social-icon" href="https://www.linkedin.com/in/mandar-bavdane-bb1ab9283/">
+          <img
+            src="/assets/linkedin.svg"
+            alt="linkedin"
+            className="w-1/2 h-1/2"
+          />
+        </a>
+        <a className="social-icon" href="">
+          <img
+            src="/assets/twitterr.svg"
+            alt="twitter"
+            className="w-1/2 h-1/2"
+          />
+        </a>
+      </div>
+      <p className="text-white-500 ">@2025 Mandar. All rights reserved.</p>
+    </section>
+  );
+};
+
+export default Footer;
