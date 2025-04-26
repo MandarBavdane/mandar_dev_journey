1db5208edce0024ce1dadc5d8a8bd3773a43785d
Mandar Bavdane <mandarbavdane@gmail.com>
2025-04-26 12:18:11 +0530

    build errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
 import Experience from "./sections/Experience";
 import Footer from "./sections/Footer";
-import Hero from "./sections/hero";
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
 
